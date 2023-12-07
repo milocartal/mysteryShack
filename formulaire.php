428ad7c9a15974424579780fdbbbbffdcc91f8ad
Ososomalfoy <diryoceane@gmail.com>
2023-12-07 18:29:09 +0100

    correction fautes et ajout du php

---
+++ formulaire.php
@@ -0,0 +1,7 @@
+<?php
+echo "Email : ".$_post['email'];
+echo "<br>Date de naissance : ".$_post['date'];
+echo "<br>sexe : ".$_post['sexe'];
+echo "<br>Email : ".$_post['email'];
+echo "<br>Message : ".$_post['message'];
+?>
